41e5e30de3eb7269dfc4ce63eba55bc01fea9dce
Luuxis-tuto <61792917+luuxis@users.noreply.github.com>
2022-06-25 08:48:54 +0200

    Update index.js

--- test/index.js
+++ test/index.js
@@ -5,11 +5,12 @@ let URL = 'http://textures.minecraft.net/texture/ac3de5b50fb6174da51032677ead046
 
 async function main() {
     const headFile1 = await jimp.read(URL);
+    const headFile2 = await jimp.read(URL);
 
     let head_crop = headFile1.crop(8, 8, 8, 8);
-    let head_crop_2 = headFile1.crop(40, 8, 8, 8);
+    let head_crop_2 = headFile2.crop(40, 8, 8, 8);
 
-    let head = head_crop.composite(head_crop_2, 0, 0).write('head.png')
+    head_crop.composite(head_crop_2, 0, 0).write('head.png')
 }
 
 
