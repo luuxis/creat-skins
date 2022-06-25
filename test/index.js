af29470a3b885a6e276e6537961e1ae5e0d8dff0
Luuxis-tuto <61792917+luuxis@users.noreply.github.com>
2022-06-25 08:35:00 +0200

    .

---
+++ test/index.js
@@ -0,0 +1,16 @@
+const jimp = require('jimp');
+
+let URL = 'http://textures.minecraft.net/texture/ac3de5b50fb6174da51032677ead046295486bf682b3ea73e0617a210c4b4f46';
+
+
+async function main() {
+    const headFile1 = await jimp.read(URL);
+
+    let head_crop = headFile1.crop(8, 8, 8, 8);
+    let head_crop_2 = headFile1.crop(40, 8, 8, 8);
+
+    let head = head_crop.composite(head_crop_2, 0, 0).write('head.png')
+}
+
+
+main();
